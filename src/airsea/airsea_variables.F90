cbdc189c27335be253bc0b9f2bde359a3586c9bf
Jon Wolfe <jonbob@lanl.gov>
2025-07-11 14:26:39 -0500

    Update Stefan-Boltzmann constant to be consistent with e3sm

--- src/airsea/airsea_variables.F90
+++ src/airsea/airsea_variables.F90
@@ -21,7 +21,8 @@
    REALTYPE, public, parameter         :: cpa=1008.
    REALTYPE, public, parameter         :: cpw=3985.
    REALTYPE, public, parameter         :: emiss=0.97
-   REALTYPE, public, parameter         :: bolz=5.67e-8
+   REALTYPE, public, parameter         :: bolz=5.670374419e-8 ! Stefan-Boltzmann constant
+                                                              ! [W/m^2/K^4]
    REALTYPE, public, parameter         :: kelvin=273.16
    REALTYPE, public, parameter         :: const06=0.62198
    REALTYPE, public, parameter         :: rgas = 287.1    !
